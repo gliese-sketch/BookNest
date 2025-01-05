16021dcd473a1b91f5dd5073d4f0dd37903877e8
gliese-sketch <173663566+gliese-sketch@users.noreply.github.com>
2025-01-05 09:32:35 +0000

    Move namesReducer to utils

--- components/Test.jsx
+++ components/Test.jsx
@@ -1,38 +1,7 @@
 "use client";
 import { useReducer } from "react";
 import Form from "./Form";
-
-const namesReducer = (prevState, action) => {
-  switch (action.type) {
-    case "ADD":
-      const newName = {
-        id: crypto.randomUUID(),
-        likes: 0,
-        text: action.payload,
-      };
-      return [...prevState, newName];
-    case "DELETE":
-      return prevState.filter((nameObj) => nameObj.id !== action.payload);
-    case "SORT":
-      return [...prevState].sort((a, b) => b.likes - a.likes);
-    case "LIKE":
-      return prevState.map((nameObj) =>
-        nameObj.id === action.payload
-          ? { ...nameObj, likes: nameObj.likes + 1 }
-          : nameObj
-      );
-    case "DISLIKE":
-      return prevState.map((nameObj) =>
-        nameObj.id === action.payload
-          ? { ...nameObj, likes: nameObj.likes - 1 }
-          : nameObj
-      );
-    case "ORDER":
-      return [...prevState].sort((a, b) => a.text.localeCompare(b.text));
-    default:
-      return prevState;
-  }
-};
+import { namesReducer } from "@/utils/namesReducer";
 
 function Test() {
   const [names, dispatch] = useReducer(namesReducer, []);
