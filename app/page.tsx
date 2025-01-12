b216e9c39ca6e8314f6eee167da191e5ffda021f
gliese-sketch <173663566+gliese-sketch@users.noreply.github.com>
2025-01-12 07:46:02 +0000

    add AddBook component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import AddBook from "@/components/AddBook";
 import Header from "@/components/Header";
 import { HeroHighlight } from "@/components/Hero";
 
@@ -8,7 +11,7 @@ function Page() {
       <HeroHighlight>
         <h1 className="text-2xl sm:text-4xl md:text-6xl">BookNest</h1>
       </HeroHighlight>
-      <p>Hey there</p>
+      <AddBook />
     </div>
   );
 }
--- components/AddBook.tsx
+++ components/AddBook.tsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { FormEvent, useRef } from "react";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
 
 function AddBook() {
-  return <div>AddBook</div>;
+  const bookTitle = useRef(null);
+  const bookAuthor = useRef(null);
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+
+    const bookTitleValue = bookTitle.current.value;
+    const bookAuthorValue = bookAuthor.current.value;
+
+    console.log(bookTitleValue, bookAuthorValue);
+  };
+
+  return (
+    <form
+      className="max-w-96 mx-auto my-10 flex flex-col gap-2"
+      onSubmit={handleSubmit}
+    >
+      <Input placeholder="Book title" ref={bookTitle} required />
+      <Input placeholder="Author" ref={bookAuthor} required />
+      <div className="flex gap-2">
+        <Button variant="outline">Sort</Button>
+        <Button>Add</Button>
+      </div>
+    </form>
+  );
 }
 
 export default AddBook;
