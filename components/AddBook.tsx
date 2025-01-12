65edbb0f641fdfdf198927b396ef02bfb130ef93
gliese-sketch <173663566+gliese-sketch@users.noreply.github.com>
2025-01-12 07:52:20 +0000

    add BookAdd component & change to Blue theme

--- components/AddBook.tsx
+++ components/AddBook.tsx
@@ -1,18 +1,25 @@
 import React, { FormEvent, useRef } from "react";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
+import { ArrowDown01Icon, CrossIcon } from "lucide-react";
 
 function AddBook() {
-  const bookTitle = useRef(null);
-  const bookAuthor = useRef(null);
+  const bookTitle = useRef<HTMLInputElement>(null);
+  const bookAuthor = useRef<HTMLInputElement>(null);
 
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
+
+    if (!bookTitle.current || !bookAuthor.current) return;
 
     const bookTitleValue = bookTitle.current.value;
     const bookAuthorValue = bookAuthor.current.value;
 
     console.log(bookTitleValue, bookAuthorValue);
+
+    // Empty the input fields
+    bookTitle.current.value = "";
+    bookAuthor.current.value = "";
   };
 
   return (
@@ -23,8 +30,12 @@ function AddBook() {
       <Input placeholder="Book title" ref={bookTitle} required />
       <Input placeholder="Author" ref={bookAuthor} required />
       <div className="flex gap-2">
-        <Button variant="outline">Sort</Button>
-        <Button>Add</Button>
+        <Button variant="outline" type="button">
+          <ArrowDown01Icon /> Sort
+        </Button>
+        <Button type="submit">
+          <CrossIcon /> Add
+        </Button>
       </div>
     </form>
   );
