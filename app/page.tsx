d59d87e63926d7233af4d8db02e879bb43e5c97e
gliese-sketch <173663566+gliese-sketch@users.noreply.github.com>
2025-01-12 08:52:46 +0000

    add reducer files

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
+import { useReducer } from "react";
+import { booksReducer } from "@/lib/reducers";
 import AddBook from "@/components/AddBook";
 import Header from "@/components/Header";
 import { HeroHighlight } from "@/components/Hero";
 import ListBooks from "@/components/ListBooks";
 
-const books = [
+const initBooks = [
   {
     id: "1",
     title: "React",
@@ -21,6 +23,8 @@ const books = [
 ];
 
 function Page() {
+  const [books, dispatch] = useReducer(booksReducer, initBooks);
+
   return (
     <div>
       <Header />
---
+++ lib/reducers.ts
@@ -0,0 +1,1 @@
+export const booksReducer = (prevState: Book[], action: Action) => {};
